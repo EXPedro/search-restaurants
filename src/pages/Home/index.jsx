872ec022abc85330e84526b3eb2d3f212e611b2a
EXPedro <pedroelisio555@gmail.com>
2021-08-27 14:29:33 -0300

    created TextField for restaurant search

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
-import { Container} from './styles'
+import React, {useState} from 'react';
+import TextField, { Input} from '@material/react-text-field';
 
-const Home = () => <Container>Hello World</Container>;
+import logo from '../../assets/logo-resized.svg';
+
+import { Container, Search} from './styles'
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+    return(
+        <Container>
+            <Search>
+                <img src = {logo} alt= "Logo do restaurante" />
+                <TextField
+                    label='Pesquisar'
+                    outlined
+                    //onTrailingIconSelect={() => this.setState({value: ''})}
+                    //trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+                    ><Input
+                    value={inputValue}
+                    onChange={(e) => setInputValue({value: e.currentTarget.value})} />
+                    </TextField>
+            </Search>
+        </Container>
+    )
+};
 
 export default Home;
