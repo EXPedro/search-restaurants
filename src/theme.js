b18e9471d1e74aa9e8af971284550961ae990f44
EXPedro <pedroelisio555@gmail.com>
2021-08-27 09:49:02 -0300

    first commit

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colrs:{
+        primary: '#6200ee',
+        background: '#00000014',
+        text: '#000000B3',
+        succes: '#135E01',
+    },
+    fonts:{
+        fontFamily: 'Roboto, sans-serif',
+    },
+};
+
+export default theme;
