64f234d66b84ce15fc6fa3fc1282ce2f97c873ca
EXPedro <pedroelisio555@gmail.com>
2021-08-27 13:47:14 -0300

    created theme, styles, reset styles and index of Home

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,9 @@
 const theme = {
-    colrs:{
+    colors:{
         primary: '#6200ee',
         background: '#00000014',
         text: '#000000B3',
-        succes: '#135E01',
+        success: '#135E01',
     },
     fonts:{
         regular: 'Roboto, sans-serif',
