56d2ffe5daee29201e5a122ddeba0d8096ec0fa7
EXPedro <pedroelisio555@gmail.com>
2021-09-01 19:51:43 -0300

    created Modal component

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -10,7 +10,7 @@ export const Container = styled.aside`
     background-color:  ${(props) => props.theme.colors.background};
     width: 360px;
     height: 100vh;
-    overflow-y: scroll;
+    overflow-y: auto;
 `;
 
 export const Search = styled.section`
