3a1e4a81b6c7e29c5643b8fbace193fc794d3fda
EXPedro <pedroelisio555@gmail.com>
2021-09-01 14:14:52 -0300

    created RestaurantInfo, Title and Address styles

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-import { Restaurant } from './styles';
+import { Restaurant, RestaurantInfo, Title, Address } from './styles';
 
-const RestaurantCard = () => <Restaurant />;
+const RestaurantCard = () => 
+<Restaurant>
+    <RestaurantInfo>
+        <Title>Nome do Restaurante</Title>
+        <p>Avaliação</p>
+        <Address>Rua do Caralho, 100</Address>
+    </RestaurantInfo>
+</Restaurant>;
 
 export default RestaurantCard ;
