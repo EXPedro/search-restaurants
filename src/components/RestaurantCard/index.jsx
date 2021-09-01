6ac5a5fcab004b15eb11ebe2599a0753c7cc28ea
EXPedro <pedroelisio555@gmail.com>
2021-09-01 16:37:41 -0300

    created RestaurantPhoto style and finished RestaurantCard

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import ReactStars from "react-rating-stars-component";
 
-import { Restaurant, RestaurantInfo, Title, Address } from './styles';
+import restaurante from '../../assets/restaurante-fake.png';
+
+import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles';
 
 const RestaurantCard = () => 
 <Restaurant>
     <RestaurantInfo>
         <Title>Nome do Restaurante</Title>
-        <p>Avaliação</p>
+        <ReactStars count={5} isHalf value={4} edit={false} activeColor="#e7711c" />
         <Address>Rua do Caralho, 100</Address>
     </RestaurantInfo>
+    <RestaurantPhoto src={restaurante} alt="foto do restaurante" />
 </Restaurant>;
 
 export default RestaurantCard ;
