24cd52f1cd6a8644d4c416cac33523a62a982fdc
EXPedro <pedroelisio555@gmail.com>
2021-09-02 15:36:05 -0300

    created onMapReady

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,7 +10,9 @@ import { Container, Carousel, Search, Logo, Wrapper, CarouselTitle} from './styl
 
 const Home= () => {
     const [inputValue, setInputValue]= useState('');
+    const [query, setQuery] = useState(null);
     const [modalOpened, setModalOpened]= useState(true);
+
     const settings = {
         dots: false,
         infinite: true,
@@ -20,6 +22,13 @@ const Home= () => {
         slidesToScroll: 4,
         adaptativeHeight: true,
     };
+
+    function handleKeyPress(e){
+        if(e.key==='Enter'){
+            setQuery(inputValue);
+        }
+    }
+
     return(
         <Wrapper>
             <Container>
@@ -28,10 +37,10 @@ const Home= () => {
                     <TextField
                         label= 'Pesquisar Restaurante'
                         outlined
-                        trailingIcon= {<MaterialIcon role = "button" icon = "search" />}
-                        >
+                        trailingIcon= {<MaterialIcon role = "button" icon = "search" />} >
                         <Input
                             value= {inputValue}
+                            onKeyPress={handleKeyPress}
                             onChange= {(e)=> setInputValue({value: e.target.value})} />
                     </TextField>
                     <CarouselTitle>Na sua área</CarouselTitle>
@@ -47,7 +56,7 @@ const Home= () => {
                 </Search>
                 <RestaurantCard />
             </Container>
-            <Map />
+            <Map query={query} />
             <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />
         </Wrapper>
     )
