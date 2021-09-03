72ff41819c3106fdf6466e3d120196ddd2937d89
EXPedro <pedroelisio555@gmail.com>
2021-09-03 20:17:46 -0300

    updated restaurantCard and imageCard

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactStars from "react-rating-stars-component";
 
 import restaurante from '../../assets/restaurante-fake.png';
+import Skeleton from '../Skeleton';
 
 import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles';
 
 const RestaurantCard = ({ restaurant, onClick }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
     return(
         <Restaurant onClick={onClick}>
             <RestaurantInfo>
@@ -17,9 +19,12 @@ const RestaurantCard = ({ restaurant, onClick }) => {
                             activeColor="#e7711c" />
                 <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
             </RestaurantInfo>
-            <RestaurantPhoto 
-                src={restaurant.photos ? restaurant.photos[0].getUrl(): restaurante} 
+            <RestaurantPhoto
+                imageLoaded={imageLoaded}
+                src={restaurant.photos ? restaurant.photos[0].getUrl(): restaurante}
+                onLoad={() => setImageLoaded(true)} 
                 alt="foto do restaurante" />
+                {!imageLoaded && <Skeleton width="100px" height="100px" />}
         </Restaurant>
     );
 };
